9d3a7c9fdfff084d88b8d6bdb9695153939a8052
David <davidmwathi412@gmail.com>
2024-10-06 21:26:34 +0300

    Made changes to App.jsx code

--- book-library-app/src/App.jsx
+++ book-library-app/src/App.jsx
@@ -3,9 +3,7 @@ import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import BookCard from './components/BookCard';
 import BookDetails from './components/BookDetails';
-const page=()=>{
-  <p> <page/> </p>
-}
+
 const App = () => {
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
@@ -26,10 +24,9 @@ const App = () => {
   return (
     
     <div className='m-11'>
-      <header className='items-center'>
-      
-         <h1 className=' pb-11 pt-5 pl-32'>📔 Online Library </h1>
-      </header>
+         <header>  
+         <h1 className=' pb-11 pt-5 pl-32'>📔 Online Library </h1></header>
+    
       <div className='bg-[url(https://th.bing.com/th/id/OIP.NjNr7MpHQ4jOsrbYGfo-VAHaE5?rs=1&pid=ImgDetMain)] bg-gradient bg-cover items-center justify-center h-screen'>
 
         <div className='text-white p-53 mr-19 ml-11  px-36 pt-24'>
@@ -49,6 +46,7 @@ const App = () => {
          <div className='mr-12 ml-13 mt-76 p-20 text-blue-600 text-center'><h1>Enjoy studying and exploring more books📚</h1></div>
          <div className=''> <Footer/> </div>
         </div>
+        
         
     </div>
 
--- book-library-app/src/components/BookDetails.jsx
+++ book-library-app/src/components/BookDetails.jsx
@@ -1,5 +1,5 @@
 const BookDetails = ({ book, onClose }) => (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center">
       <div className="bg-white p-2 rounded mb-12 mt-12 h-auto shadow-lg max-w-lg w-full">
         <button onClick={onClose} className="absolute ml-96 text-gray-500 hover:text-gray-700">&times;</button>
         <h2 className="text-2xl font-bold mb-4">{book.title}</h2>
